9665fda7a75a825ea61b3cc7af096e3a24712d8c
0537ch <abrar.ikramaputra@gmail.com>
2024-03-17 01:13:22 +0700

    4rd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/index', function () {
+    return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/product', function () {
+    return view('product');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/account', function () {
+    return view('account');
 });
